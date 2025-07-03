1e4a130c25f3fcf76bd47c613de910229ee3f59f
Aravsharm <sv2480057@gmail.com>
2025-07-03 09:23:42 +0000

    dataTypes

---
+++ 01_basic/2_dataType.js
@@ -0,0 +1,24 @@
+// primitive data type
+// null is an object
+
+// number , bigint , string , boolean , null , undefined symbol (unique)
+
+// Non-primitive data type
+
+// object
+
+let Id = 12
+let name = "Sumit"
+let isName = "true"
+let value = null
+let studentId 
+
+console.log(typeof Id)
+
+console.log(typeof name)
+
+console.log(typeof isName)
+
+console.log(typeof value)
+
+console.log(typeof studentId)
