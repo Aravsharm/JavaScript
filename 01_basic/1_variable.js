be00f168b9d835f1ef1ae961df13db6672a0eb0a
Aravsharm <sv2480057@gmail.com>
2025-07-03 07:44:35 +0000

    variable

---
+++ 01_basic/1_variable.js
@@ -0,0 +1,19 @@
+const studentId = 1234
+let studentName = "Sumit"
+var studentEmail = "sumit123@gmail.com"
+studentCity = "Kanpur"
+
+// const variable can not be changed
+
+// studentId = 4211
+
+// let variable can be changed only its block scope
+studentName = "Vishwakarma"
+
+// var variable can be changed anywhere either inside block scope or not so we don't have to use it
+studentEmail = "sharma123@gmail.com"
+
+// By default var
+studentCity = "Lucknow"
+
+console.table([studentId , studentName , studentEmail , studentCity])
