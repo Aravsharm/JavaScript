5becdae2579adc91b380cf0f9499975852afb92c
Aravsharm <sv2480057@gmail.com>
2025-07-17 09:39:17 +0000

    conversionOperation

---
+++ 01_basic/3_conversionOperation.js
@@ -0,0 +1,27 @@
+let number = 33
+
+let stringNumber = String (number)
+
+// It converts the type of number but seems as a number
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+let string = "53sad"
+
+let numConvert = Number (string)
+
+console.log(numConvert); // It convert the datatype not the value it returns NaN (Not a Number)
+console.log(typeof numConvert);
+
+let boolNumber = "1"
+
+let boolConvert = Boolean (boolNumber)
+
+console.log(boolConvert); // it returns true
+console.log(typeof boolConvert);
+
+//  1 => true   &    0 => false
+//  "" => false   &   "sumit" => true
+
+
+
