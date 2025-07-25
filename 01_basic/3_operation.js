72e28b5529eae03bf83576c0764161bb924ff596
Aravsharm <sv2480057@gmail.com>
2025-07-25 03:59:53 +0000

    operations

---
+++ 01_basic/3_operation.js
@@ -0,0 +1,13 @@
+// BASIC OPERATIONS
+
+let val = 3
+
+let conVal = -val
+console.log(conVal);
+
+// We should not to write to these type of syntax rather than we have to use brackets for presidense
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
