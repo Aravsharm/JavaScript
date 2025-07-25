fc2ef28457c1e8542b925c68de904b801571bf85
Aravsharm <sv2480057@gmail.com>
2025-07-25 03:57:55 +0000

    conversion Operations

--- 01_basic/3_conversionOperation.js
+++ 01_basic/3_conversionOperation.js
@@ -25,3 +25,22 @@ console.log(typeof boolConvert);
 
 
 
+// BASIC OPERATIONS
+
+let val = 3
+
+let conVal = -val
+console.log(conVal);
+
+// We should not to write to these type of syntax rather than we have to use brackets for presidense
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+
+
+
+
